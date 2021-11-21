560b4cdee7eddd8662ffdf71e909b3de45aad332
Saj Panchal <Spanchal8146@conestogac.on.ca>
2021-11-21 12:51:28 -0500

    refactored color coding.

--- app/screens/ViewImage.js
+++ app/screens/ViewImage.js
@@ -1,48 +1,44 @@
 import React from "react";
-import { View, Image } from "react-native";
-
+import { View, Image, StyleSheet } from "react-native";
+import colors from "../config/colors";
 export default function ViewImage() {
   return (
-    <View style={{ flex: 1 }}>
-      <View
-        style={{
-          flex: 0.2,
-          backgroundColor: "black",
-          flexDirection: "row",
-          justifyContent: "center",
-        }}
-      >
-        <View
-          style={{
-            flex: 0.9,
-            backgroundColor: "black",
-            flexDirection: "row",
-            justifyContent: "space-between",
-            top: 25,
-          }}
-        >
-          <View
-            style={{
-              backgroundColor: "tomato",
-              height: 50,
-              width: 50,
-            }}
-          ></View>
-
-          <View
-            style={{
-              backgroundColor: "dodgerblue",
-              height: 50,
-              width: 50,
-            }}
-          ></View>
+    <View style={styles.mainView}>
+      <View style={styles.bannerView}>
+        <View style={styles.imagePickerView}>
+          <View style={styles.closeIcon}></View>
+          <View style={styles.deleteIcon}></View>
         </View>
       </View>
-      <Image
-        style={{ flex: 0.6, width: "100%", height: "100%" }}
-        source={require("../assets/chair.jpg")}
-      />
-      <View style={{ flex: 0.2, backgroundColor: "black" }}></View>
+      <Image style={styles.imageView} source={require("../assets/chair.jpg")} />
+      <View style={styles.bannerView}></View>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  mainView: { flex: 1 },
+  bannerView: {
+    flex: 0.2,
+    backgroundColor: colors.black,
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  imagePickerView: {
+    flex: 0.9,
+    backgroundColor: colors.black,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    top: 25,
+  },
+  closeIcon: {
+    backgroundColor: colors.primary,
+    height: 50,
+    width: 50,
+  },
+  deleteIcon: {
+    backgroundColor: colors.secondary,
+    height: 50,
+    width: 50,
+  },
+  imageView: { flex: 0.6, width: "100%", height: "100%" },
+});
